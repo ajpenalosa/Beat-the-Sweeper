e789d6bea3202c3ba5832b201954e1ccdc18500f
Anthony Penalosa <aj@Anthonys-MacBook-Pro.local>
2018-06-03 14:09:06 -0700

    Making edits to sign out button

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,90 +1,94 @@
-// Google Sign-In
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-    console.log(profile);
+$(document).ready(function() {
 
-    // Dashboard
+  // Google Sign-In
+  function onSignIn(googleUser) {
+      // Useful data for your client-side scripts:
+      var profile = googleUser.getBasicProfile();
+      console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log("Image URL: " + profile.getImageUrl());
+      console.log("Email: " + profile.getEmail());
+      // The ID token you need to pass to your backend:
+      var id_token = googleUser.getAuthResponse().id_token;
+      console.log("ID Token: " + id_token);
+      console.log(profile);
 
-    // Hides sign in message
-    $(".dashboard-message").hide();
+      // Dashboard
 
-    // Reference to dashboard div
-    var dashboard = $(".dashboard");
+      // Hides sign in message
+      $(".dashboard-message").hide();
 
-    // Creating user info row
-    var userInfoRow = $("<div class='row'>");
+      // Reference to dashboard div
+      var dashboard = $(".dashboard");
 
-    // Creating profile image column
-    var imageColumn = $("<div class='col-sm-2 text-center'>");
-    var image = $("<img>").attr("src", profile.getImageUrl());
+      // Creating user info row
+      var userInfoRow = $("<div class='row'>");
 
-    // Appending profile image column to dashboard
-    userInfoRow.append(imageColumn);
-    imageColumn.append(image);
-    dashboard.append(userInfoRow);
+      // Creating profile image column
+      var imageColumn = $("<div class='col-sm-2 text-center'>");
+      var image = $("<img>").attr("src", profile.getImageUrl());
 
-    // Creating user info column
-    var userInfoColumn = $("<div class='col-sm-10'>");
-    var fullNameP = $("<p>");
-    fullNameP.text("Name: " + profile.getName());
-    var userEmailP = $("<p>");
-    userEmailP.text("E-Mail: " + profile.getEmail());
+      // Appending profile image column to dashboard
+      userInfoRow.append(imageColumn);
+      imageColumn.append(image);
+      dashboard.append(userInfoRow);
 
-    // Appending user info column to row
-    userInfoRow.append(userInfoColumn);
-    userInfoColumn.append(fullNameP, userEmailP);
+      // Creating user info column
+      var userInfoColumn = $("<div class='col-sm-10'>");
+      var fullNameP = $("<p>");
+      fullNameP.text("Name: " + profile.getName());
+      var userEmailP = $("<p>");
+      userEmailP.text("E-Mail: " + profile.getEmail());
 
-    // Creating sign out button
-    var navBar = $(".navbar-nav");
-    var signOutLi = $("<li class='nav-item'>")
-    var signOutLink = $("<a class='nav-link' href='#' onclick='signOut();'>").text("Sign Out");
+      // Appending user info column to row
+      userInfoRow.append(userInfoColumn);
+      userInfoColumn.append(fullNameP, userEmailP);
 
-    navBar.append(signOutLi);
-    signOutLi.append(signOutLink);
-};
+      // Shows sign-out button
+      $(".sign-out-link").show();
+  };
 
-// Google Sign Out
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    // Shows sign in message
-    $(".dashboard-message").show();
-      $(".dashboard").empty();
-    console.log('User signed out.');
+  // Hides sign-out button
+  $(".sign-out-link").hide();
+
+  // Google Sign Out
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      // Hides sign-out button
+      $(".sign-out-link").hide();
+      // Shows sign in message
+      $(".dashboard-message").show();
+        $(".dashboard").empty();
+      console.log('User signed out.');
+    });
+  }
+
+  // Street Sweeping API
+
+  $.ajax({
+      url: "https://data.lacity.org/resource/x8i3-2x54.json",
+      type: "GET",
+      data: {
+        "$limit" : 5000,
+        "$$app_token" : "aWDcPjXSGOOSmKIk1wuZzfykV"
+      }
+  }).done(function(data) {
+    console.log(data);
   });
-}
-
-// Street Sweeping API
-
-$.ajax({
-    url: "https://data.lacity.org/resource/x8i3-2x54.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-      "$$app_token" : "aWDcPjXSGOOSmKIk1wuZzfykV"
-    }
-}).done(function(data) {
-  console.log(data);
-});
 
 
-// google maps GeoLocation API
-var queryMapURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAnuOLy1xpmpNsIvSH165kWQYKAvlQqnz4"
+  // google maps GeoLocation API
+  var queryMapURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAnuOLy1xpmpNsIvSH165kWQYKAvlQqnz4"
 
-$.ajax({
-    url: queryMapURL,
-    method: "POST"
+  $.ajax({
+      url: queryMapURL,
+      method: "POST"
 
-}).then(function(response) {
-    console.log(response)
-});
+  }).then(function(response) {
+      console.log(response)
+  });
+  
+}); // End of Document.ready
